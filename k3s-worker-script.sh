1ff9987eb37ba881e22a3cc105d4b46c1d7f0f06
Antoine CHOULA <76167680+kevAnto@users.noreply.github.com>
2024-09-22 14:31:19 +0200

    Node Script

--- k3s-worker-script.sh
+++ k3s-worker-script.sh
@@ -7,9 +7,15 @@ echo "Updating and upgrading the system..."
 sudo apt-get update -y
 sudo apt-get upgrade -y
 
+sudo su
 apt update
 apt install docker.io -y
+systemctl status docker
 
-hostnamectl set-hostname worker-node-1
-echo worker-node-1 > /etc/hostname
-#kubectl delete node worker-node-1
+# curl -sfL https://get.k3s.io | K3S_URL=https://<MasterNodePublicIP>:6443 K3S_TOKEN=<NodeToken> sh -
+
+# kubectl get nodes
+
+# hostnamectl set-hostname worker-node-1
+# echo worker-node-1 > /etc/hostname
+# kubectl delete node worker-node-1
